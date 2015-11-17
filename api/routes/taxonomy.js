c6d2e96fd031a4bc44db42769602bdfcd1e053ab
hame25 <hamishhamer112@hotmail.com>
2015-11-17 22:08:55 +0000

    real taxonomy endpoint

--- api/routes/taxonomy.js
+++ api/routes/taxonomy.js
@@ -1,14 +1,31 @@
 import Express from 'express';
+import fetch from 'isomorphic-fetch';
+import cheerio from 'cheerio';
 
 let router = Express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json({
-  	taxonomy: [
-  		{name: 'Technology', url: 'wwww.google.com'},
-  		{name: 'Home and garden', url: 'www.facebook.com'}
-  	]
-  });
+  
+	fetch('http://www.tesco.com/direct/')
+		.then((response) => {
+			return response.text();
+		})
+		.then((output) => {
+			let $ = cheerio.load(output);
+
+			let taxonomyArray = [];
+
+			$('#catalogue-nav .departments > li:not(.first) > a').each(function (i, element) {
+				let item = {};
+
+				item.name = this.children[0].data;
+				item.url = 'wwww.google.com';
+
+				taxonomyArray.push(item);
+
+			})
+			res.send({taxonomy: taxonomyArray});
+		});
 });
 
 export default router;
